ba7119536fdddf837f726786b43c9cdea2fac3b6
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-06-29 23:53:53 +0800

    feat: href

--- src/utils/link.ts
+++ src/utils/link.ts
@@ -1,5 +1,5 @@
 import type { PoliciesAsset } from "user-attachments"
 
 export default (returnObj: PoliciesAsset) => {
-  alert(`该订阅的导入链接是：https://share.adproqwq.top/share/${returnObj.id}`);
+  alert(`该订阅的下载链接是：https://share.adproqwq.top/share/${returnObj.id}\r该订阅的导入链接是：${returnObj.href}`);
 };
