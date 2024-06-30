3356887f04d77dcd20be82fe28d08879c7c52f6f
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-06-30 19:10:28 +0800

    perf: origin

--- src/utils/link.ts
+++ src/utils/link.ts
@@ -1,7 +1,6 @@
 import type { PoliciesAsset } from 'user-attachments';
 
 export default (returnObj: PoliciesAsset) => {
-  (document.getElementById('link') as HTMLParagraphElement)
-    .innerHTML = `下载链接：https://share.adproqwq.top/share/${returnObj.id}\r导入链接：${returnObj.href}`;
+  (document.getElementById('link') as HTMLParagraphElement).innerHTML = `导入链接：${window.location.origin}/share/${returnObj.id}`;
   (document.getElementById('link') as HTMLParagraphElement).style.display = 'block';
 };
