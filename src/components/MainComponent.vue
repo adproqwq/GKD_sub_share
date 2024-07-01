a652f3b1376d605360f4d55857af6436dae2c939
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-07-01 20:13:14 +0800

    feat: tutoril

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,5 +1,4 @@
 <script lang="ts">
-import { RouterLink } from 'vue-router';
 import transfer from '../utils/transfer';
 import read from '../utils/read';
 import { defineComponent } from 'vue';
@@ -23,24 +22,24 @@ export default defineComponent({
       <h2>GKD规则分享</h2>
       <textarea id="gkd-sub" rows="15" cols="100" placeholder="请输入GKD规则&#10;它应为订阅"></textarea>
       <button class="button" @click="transfer">获取订阅链接</button>
-      <input class="button" type="file" accept=".json, .json5" @change="uploadFile">上传订阅文件</input>
+      <input class="button" type="file" accept=".json, .json5" @change="uploadFile"></input>
       <p class="message" id="link" style="display: none;"></p>
       <p class="message" id="export-message">
         本网页使用教程：
-        <RouterLink to="/tutoril">点此查看使用教程（施工中）</RouterLink>
+        <a href="https://mcnqj6ctiznc.feishu.cn/wiki/TwWFwhpNtiqSAskTTBscKzDNnmf">点此查看使用教程</a>
       </p>
     </div>
 
     <footer>
       <p align="center">
         <b>
-          点击查看<a href="https://github.com/adproqwq/GKD_sub_share" target="_blank"> Github </a>项目
+          点击查看<a href="https://github.com/adproqwq/GKD_sub_share" target="_blank"> Github </a>项目<br>
           本页面由<a href="https://github.com/adproqwq" target="_blank"> adproqwq </a>进行维护
         </b>
       </p>
       <p align="center">
         <b>
-          本页面根据 https://snoopy1866.github.io/LiTiaotiao-Custom-Rules/ 本人使用Vue重构并修改而来
+          本页面根据 https://snoopy1866.github.io/LiTiaotiao-Custom-Rules/ 本人使用Vue重构并修改而来<br>
           在此特别感谢<a href="https://github.com/FW27623"> FW27623 </a>
         </b>
       </p>
