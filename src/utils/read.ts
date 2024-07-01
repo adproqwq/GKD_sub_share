3895f165277b26d4a87d3058b7ded93f2300f4b4
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-07-01 21:25:04 +0800

    perf: error alert

--- src/utils/read.ts
+++ src/utils/read.ts
@@ -1,5 +1,10 @@
 import json5 from 'json5';
 
 export default async (file: File) => {
+  try{
+    json5.stringify(json5.parse(await file.text()), undefined, 2);
+  } catch{
+    alert('JSON解析失败');
+  }
   (document.getElementById('gkd-sub') as HTMLTextAreaElement).value = json5.stringify(json5.parse(await file.text()), undefined, 2);
 };
