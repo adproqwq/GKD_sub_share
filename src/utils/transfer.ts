05c5e1a27b87dccd40217e7ff0df931d5f13c3fd
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-07-04 14:53:19 +0800

    perf: no script tips

--- src/utils/transfer.ts
+++ src/utils/transfer.ts
@@ -3,12 +3,17 @@ import link from './link';
 import json5 from 'json5';
 
 export default async () => {
-  link(
-    await upload(
+  let policiesAsset;
+  try{
+    policiesAsset = await upload(
       new File(
         [new Blob([JSON.stringify(json5.parse((document.getElementById('gkd-sub') as HTMLTextAreaElement).value))])],
         'sub.json'
       )
-    )
-  );
+    );
+  } catch{
+    alert('上传失败。请确认已正确安装油猴脚本并已向本网站注入GM_XHR。');
+    return;
+  }
+  link(policiesAsset);
 };
