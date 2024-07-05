ab1eedb16e996b1bf7596cbac44e3bdf6877ad13
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-07-06 01:13:23 +0800

    perf: error alert

--- src/utils/transfer.ts
+++ src/utils/transfer.ts
@@ -11,8 +11,9 @@ export default async () => {
         'sub.json'
       )
     );
-  } catch{
+  } catch(e){
     alert('上传失败。请确认已正确安装油猴脚本并已向本网站注入GM_XHR。');
+    alert(`报错信息如下，请在反馈问题时将本弹窗截图一并提供：${e}`);
     return;
   }
   link(policiesAsset);
