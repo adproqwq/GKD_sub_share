24b8064619b0c9bd2a704a819f7ea718953bcfef
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-07-07 10:55:44 +0800

    docs: github

--- src/utils/transfer.ts
+++ src/utils/transfer.ts
@@ -12,7 +12,7 @@ export default async () => {
       )
     );
   } catch(e){
-    alert('上传失败。请确认已正确安装油猴脚本并已向本网站注入GM_XHR。');
+    alert('上传失败。请确认已正确安装油猴脚本并已向本网站注入GM_XHR，且已登录Github。');
     alert(`报错信息如下，请在反馈问题时将本弹窗截图一并提供：${e}`);
     return;
   }
